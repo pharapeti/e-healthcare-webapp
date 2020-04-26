a71322568a483e7f01619fcac339bc7e5fa14bc1
pharapeti <patrice.harapeti@gmail.com>
2020-04-26 22:57:26 +1000

    Use user as simpler association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   validates :username, uniqueness: true, presence: true
+  has_many :chat_messages
 end
