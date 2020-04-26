6e756d62f38fc896ae5c327bc4a0ea183e7dfe54
pharapeti <patrice.harapeti@gmail.com>
2020-04-26 23:38:52 +1000

    More cleanup

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  belongs_to :answered_by, class_name: 'Doctor'
+  belongs_to :answered_by, class_name: 'Doctor', foreign_key: 'answered_by_id'
 
   validates_presence_of :question, :answered_by
 end
