e10a90d949ac16d707beaafa312ab0df04686911
pharapeti <patrice.harapeti@gmail.com>
2020-04-27 21:26:58 +1000

    Organise all files

--- app/helpers/application_helper.rb
+++
@@ -1,6 +0,0 @@
-module ApplicationHelper
-  def gravatar_url(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    url = "https://gravatar.com/avatar/#{gravatar_id}.png"
-  end
-end
--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-
-  validates :username, uniqueness: true, presence: true
-  has_many :chat_messages
-end
