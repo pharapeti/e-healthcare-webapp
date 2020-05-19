5933ac9291e982f371e54a8987b155cf251f69cd
Yunwei-Zhang <62420557+Yunwei-Zhang@users.noreply.github.com>
2020-05-19 11:54:41 +0800

    patient creates urgent request

--- app/controllers/urgent_requests_controller.rb
+++ app/controllers/urgent_requests_controller.rb
@@ -1,5 +1,8 @@
-class UrgentRequestsController < RequestsController
+class UrgentRequestsController < ApplicationController
+  layout 'patients/patients'
+  skip_before_action :verify_authenticity_token
   before_action :set_urgent_request, only: %i[show edit update destroy]
+  before_action :set_patient
 
   def index
     @urgent_requests = UrgentRequest.where(patient: @patient)
@@ -9,9 +12,22 @@ class UrgentRequestsController < RequestsController
   end
 
   def new
+    @urgent_request = UrgentRequest.new
   end
 
   def create
+    @urgent_request = UrgentRequest.new(urgent_request_params)
+    @urgent_request.patient = current_user.patient
+
+    respond_to do |format|
+      if @urgent_request.save
+        format.html { redirect_to patient_urgent_request_path(id: @urgent_request), notice: 'Urgent Request was successfully created.' }
+        format.json { render :show, status: :created, location: @urgent_request }
+      else
+        format.html { render :new }
+        format.json { render json: @urgent_request.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -28,6 +44,12 @@ class UrgentRequestsController < RequestsController
     def set_urgent_request
       @urgent_request = UrgentRequest.find(params[:id])
     end
+
+
+
+  def set_patient
+    @patient = current_user.patient
+  end
 
     # Only allow a list of trusted parameters through.
     def urgent_request_params
