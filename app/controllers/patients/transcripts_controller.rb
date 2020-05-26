435219c449de1bd5dd1fb45870e0d5c2c613dacc
Johan Do <13557792@student.uts.edu.au>
2020-05-26 22:36:28 +1000

    Added Transcript View (Without Chat)

--- app/controllers/patients/transcripts_controller.rb
+++ app/controllers/patients/transcripts_controller.rb
@@ -1,5 +1,10 @@
 class Patients::TranscriptsController < PatientsController
+  before_action :set_transcript, only: [:show]
   def index 
       @transcripts = Transcript.joins(:chat_room).where(chat_rooms: { patient: current_user.patient })
+  end
+  
+  def set_transcript
+    @transcript = Transcript.find(params[:id])
   end
 end
