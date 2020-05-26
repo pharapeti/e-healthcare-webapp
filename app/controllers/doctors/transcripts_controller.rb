ab7ea970ef7a7b2ea640659c742c2c2e35e519a5
Johan Do <13557792@student.uts.edu.au>
2020-05-27 00:11:20 +1000

    Resloved Transcript view (No chat)

--- app/controllers/doctors/transcripts_controller.rb
+++ app/controllers/doctors/transcripts_controller.rb
@@ -1,5 +1,10 @@
 class Doctors::TranscriptsController < DoctorsController
+  before_action :set_transcript, only: [:show]
   def index
     @transcripts = Transcript.joins(:chat_room).where(chat_rooms: { doctor: current_user.doctor })
+  end
+
+  def set_transcript
+    @transcript = Transcript.find(params[:id])
   end
 end
